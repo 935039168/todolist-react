6575afd7572040b7d95d0b0a1da475a419e749d1
Zhaolu Guo <guozhaolu2008@163.com>
2021-12-01 23:13:38 +0800

    样式优化

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 
 class TodoItem extends Component {
-    constructor(props) {
-        super(props);
-        this.handleItemClick = this.handleItemClick.bind(this);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.handleItemClick = this.handleItemClick.bind(this);
+    this.state = {};
+  }
 
-    handleItemClick() {
-        // 调用父组件传来的方法，改变父组件的list数据
-        const { deleteFunction, index } = this.props;
-        deleteFunction(index);
-    }
+  handleItemClick() {
+    // 调用父组件传来的方法，改变父组件的list数据
+    const { deleteFunction, index } = this.props;
+    deleteFunction(index);
+  }
 
-    render() {
-        // 子组件通过this.props属性，接收父组件传递的值
-        const { content } = this.props;
-        return <li onClick={this.handleItemClick}>{content}</li>
-    }
+  render() {
+    // 子组件通过this.props属性，接收父组件传递的值
+    const { content } = this.props;
+    return <li onClick={this.handleItemClick}>{content}</li>;
+  }
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,72 +3,71 @@ import { Component } from "react";
 import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleKeyUp = this.handleKeyUp.bind(this);
-        this.handleItemClick = this.handleItemClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
 
-        this.state = {
-            inputValue: "",
-            list: []
-        }
-    }
+    this.state = {
+      inputValue: "",
+      list: [],
+    };
+  }
 
-    // 文本框输入内容
-    handleInputChange(e) {
-        this.setState({
-            inputValue: e.target.value
-        });
+  // 文本框输入内容
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+  // 支持回车键增加项目
+  handleKeyUp(e) {
+    if (e.keyCode === 13 && e.target.value !== "") {
+      const list = [...this.state.list, this.state.inputValue];
+      this.setState({
+        list,
+        inputValue: "",
+      });
     }
-    // 支持回车键增加项目
-    handleKeyUp(e) {
-        if (e.keyCode === 13 && e.target.value !== "") {
-            const list = [...this.state.list, this.state.inputValue];
-            this.setState({
-                list,
-                inputValue: ""
-            });
-        }
-    }
+  }
 
-    handleItemClick(index) {
-        const list = [...this.state.list];
-        list.splice(index, 1);
-        this.setState({ list });
-    }
+  handleItemClick(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({ list });
+  }
 
-    getListItems() {
-        return this.state.list.map((value, index) => {
-            return (
-                // 父组件通过属性向子组件传值
-                <TodoItem
-                    content={value}
-                    index={index}
-                    deleteFunction={this.handleItemClick}
-                />
-            )
-        })
-    }
+  getListItems() {
+    return this.state.list.map((value, index) => {
+      return (
+        // 父组件通过属性向子组件传值
+        <TodoItem
+          content={value}
+          index={index}
+          key={index}
+          deleteFunction={this.handleItemClick}
+        />
+      );
+    });
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <label htmlFor='myinput'>请输入待办事项：</label>
-                <input
-                    id="myinput"
-                    className="input"
-                    value={this.state.inputValue}
-                    onChange={this.handleInputChange}
-                    onKeyUp={this.handleKeyUp}
-                />
-                <ul>
-                    {this.getListItems()}
-                </ul>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <input
+          id="myinput"
+          className="input"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          onKeyUp={this.handleKeyUp}
+          placeholder={"请输入待办事项"}
+        />
+        <ul>{this.getListItems()}</ul>
+      </Fragment>
+    );
+  }
 }
 
 export default TodoList;
